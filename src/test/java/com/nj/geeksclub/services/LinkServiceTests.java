8aef54b76538fd27b566d27449f2ba7c2b54cd0a
Neha Jain <neha.jain216@gmail.com>
2018-04-05 10:44:29 +0530

    Unit Test for Link Services

--- src/test/java/com/nj/geeksclub/services/LinkServiceTests.java
+++ src/test/java/com/nj/geeksclub/services/LinkServiceTests.java
@@ -1,20 +1,22 @@
 package com.nj.geeksclub.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.verify;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.internal.creation.bytebuddy.ByteBuddyMockMaker;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.nj.geeksclub.entities.Link;
+import com.nj.geeksclub.exceptions.CustomException;
 import com.nj.geeksclub.repositories.LinkRepository;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -40,10 +42,26 @@ public class LinkServiceTests {
 	}
 	
 	@Test
-	public void should_save_the_user()
+	public void should_save_the_newlink()
 	{
-		BDDMockito.doNothing().when(linkRepository).save(new Link(1, "test1", "test1"));
-		verify(linkRepository).save(new Link(1, "test1", "test1"));
+		Link link = new Link(1, "test1", "test1");
+		BDDMockito.given(linkRepository.save(link)).willReturn(link);
+		Link saveLink = linkService.saveLink(link);
+		//assertEquals(1, saveLink.getId()); -- from JUNIT
+		assertThat(saveLink.getId()).isEqualTo(1);
+		verify(linkRepository).save(link);
+	}
+	
+	@Test
+	//(expected=CustomException.class)
+	public void should_throw_exception_for_duplicate_link()
+	{
+		Link link = new Link(1, "test1", "test1");
+		BDDMockito.given(linkRepository.findByUrl(link.getUrl())).willReturn(Optional.of(link));
+		//BDDMockito.given(linkRepository.save(link)).willThrow(new CustomException("URL already exists"));
+		//Link saveLink = linkService.saveLink(link);
+		assertThatExceptionOfType(CustomException.class).isThrownBy(() -> {linkService.saveLink(link); })
+        .withMessage("URL already exists");
 	}
 
 }
